8ef6a7b77466bf52781a70666189c1b31079224b
anonymous_object <djtaylor@gmail.com>
2025-11-14 13:29:28 -0500

    forgot the auth router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,11 +4,12 @@ API v1 Router
 
 from fastapi import APIRouter
 
-from app.api.v1 import comments, favorites, images, privmsgs, tags, users
+from app.api.v1 import auth, comments, favorites, images, privmsgs, tags, users
 
 router = APIRouter()
 
 # Include all endpoint routers
+router.include_router(auth.router)
 router.include_router(images.router)
 router.include_router(tags.router)
 router.include_router(users.router)
